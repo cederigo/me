5e08bac09eecb6b9d199896718f1677e7b5eb036
Cedric Reginster <cederigo@gmail.com>
2010-11-23 11:04:49 +0100

    - plots - find biggest eigenvektoren automatically

--- serie6/pca.m
+++ serie6/pca.m
@@ -1,4 +1,5 @@
 X = load('pcaData.txt');
+n = 3;
 %normalize data to interval [0 1]
 mins = min(X);
 mins = mins(ones(size(X,1),1),:);
@@ -15,23 +16,31 @@ X = X - mj;
 m = sum(X) / size(X,1);
 % Kovarianz-matrix
 K = (X' * X) - m'*m;
+% eigenvektoren, eigenwerte
 [V,D] = eig(K);
+D_MAX = max(D,[],2);
+%used to hold indexes of eigenvectors
+V_MAX_IND = 1:n;
+% take 3 highest eigenvalues's
+for i = V_MAX_IND
+    [val, ind] = max(D_MAX);
+    V_MAX_IND(i) = ind;    
+    D_MAX(ind) = -1;    
+end
 
-% take 3 highest eigenvalues's corresponding vectors
-% hard coded sorry
-E = V(:,[15 16 17]);
+E = V(:,V_MAX_IND);
 
 %new subspace
 Y = E' * X';
 Y = Y';
 
-%plot3(Y(:,1),Y(:,2),Y(:,3),'+b');
+plot3(Y(:,1),Y(:,2),Y(:,3),'+b');
 
 %project x-y
 %plot(Y(:,1),Y(:,2),'+b');
 
 %project x-z
-plot(Y(:,1),Y(:,3),'+b');
+%plot(Y(:,1),Y(:,3),'+b');
 
 %project y-z
 %plot(Y(:,2),Y(:,3),'+b');
